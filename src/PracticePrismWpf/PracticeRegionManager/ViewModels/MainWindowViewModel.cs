fbcc58568859670738a391b13db0ffb592e9e404
kusa-mochi <whoatemyapplepie@gmail.com>
2022-07-30 11:04:30 +0900

    shorten the code.

--- src/PracticePrismWpf/PracticeRegionManager/ViewModels/MainWindowViewModel.cs
+++ src/PracticePrismWpf/PracticeRegionManager/ViewModels/MainWindowViewModel.cs
@@ -4,12 +4,7 @@ namespace PracticeRegionManager.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+        public string Title { get; set; } = "けーねアプリ";
 
         public MainWindowViewModel()
         {
